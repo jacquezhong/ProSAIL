94a27e63a6536248f2a82af55d73216e936eca8a
力恒 <zhongliheng.zlh@alibaba-inc.com>
2024-02-15 12:33:45 +0800

    cell run

--- example.py
+++ example.py
@@ -1,16 +1,24 @@
+# %%
+import matplotlib.pyplot as plt
 # Import the model class
 from prosail import Prosail
 
 p = Prosail()
 
 results = p.run(1.5, 40, 8, 0, 0.01, 0.009, 1, 3, 0.01, 30, 10, 10, p.Planophile)
-print results
+print(results)
+plt.plot(results[0], results[1])
+plt.plot(results[0], results[2])
+plt.show()
 
 # Results ready for use with Py6S
 results2 = p.run(1.5, 40, 8, 0, 0.01, 0.009, 1, 3, 0.01, 30, 10, 10, p.Planophile, Py6S=True)
-print results2
+print(results2)
+plt.plot(results2[:, 0], results2[:, 1])
+plt.show()
 
 # Use these results with Py6S by running something like:
 # s = SixS()
 # s.ground_reflectance = GroundReflectance.HomogeneousLambertian(results2)
 # s.run()
+# %%
